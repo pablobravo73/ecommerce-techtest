ad124ad10e89ac2c71df6f84adc9b89461331ce8
pablobravo73 <pablobravo73>
2023-06-09 12:54:45 -0600

    ft: routes

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,32 @@
-//const { PORT } = require("./config/config");
-const { db } = require("./config/database")
-
-require ('dotenv').config();
+const { db } = require("./config/database");
+require('dotenv').config();
 
 const express = require("express");
 const app = express();
 
-
-
-//const express = require('express');
-//const app = express();
-//const cors = require("cors");
-
-
-
-
-
-
-
 const HOST = '0.0.0.0';
 const PORT = 3000;
 
-//app.use(cors());
-
-// Definir una ruta de ejemplo
-
 app.use(express.json());
+
+
+const inventoryRoutes = require("./routes/inventoryRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+
+const authenticateUser = require("./middleware/authenticateUser");
+
+
+app.use("/inventory", authenticateUser, inventoryRoutes);
+
+
+app.use("/auth", authRoutes);
+
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+  res.send('<h1>Hello, World!</h1>');
 });
 
 db.sync().then(async () => {
-    app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+  app.listen(PORT, HOST, () => console.log(`Server is running on ${HOST}:${PORT}`));
 });
